991b3520bce8f98b7fee9b923b1ee08cf11febf3
matejchmiel <120456681+matejchmiel@users.noreply.github.com>
2025-02-26 11:58:34 +0100

    hotovo

--- tests/kalkulacka.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from src import calculator
-
-def test_add():
-    assert calculator.add(5,3) == 8
-
---
+++ tests/test_calculator.py
@@ -0,0 +1,20 @@
+import pytest
+from src import calculator
+
+def test_add():
+    assert calculator.add(5,3) == 8
+
+def test_add_wrong():
+    assert calculator.add_wrong(5,3) == 8
+
+def test_subtract():
+    assert calculator.subtract(5,3) == 2
+
+def test_multiply():
+    assert calculator.multiply(5,3) == 15
+
+def test_divide():
+    assert calculator.divide(8,4) == 2
+
+def test_multiply_wrong():
+    assert calculator.multiply_wrong(5,3) == 15 
